bfffc87625c89c784cb09c2146aabab3ab33492b
Vindicated-Rt <dengwenhao1105@gmail.com>
2019-01-18 23:27:52 +0800

    add camera(MVP) change camera layout

---
+++ app/src/main/java/vindicatedrt/com/myapplication/Activity/Camera_Activity.java
@@ -0,0 +1,16 @@
+package vindicatedrt.com.myapplication.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import vindicatedrt.com.myapplication.R;
+
+public class Camera_Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.camera_layout);
+    }
+}
