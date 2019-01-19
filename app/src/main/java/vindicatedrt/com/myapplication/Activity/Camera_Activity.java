397761ee16abeb8b5c917f0915909ca4ff324f48
Vindicated-Rt <dengwenhao1105@gmail.com>
2019-01-20 01:40:00 +0800

    set app FULLSCREEN

--- app/src/main/java/vindicatedrt/com/myapplication/Activity/Camera_Activity.java
+++ app/src/main/java/vindicatedrt/com/myapplication/Activity/Camera_Activity.java
@@ -2,6 +2,8 @@ package vindicatedrt.com.myapplication.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import vindicatedrt.com.myapplication.R;
 
@@ -11,6 +13,9 @@ public class Camera_Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.camera_layout);
     }
 }
