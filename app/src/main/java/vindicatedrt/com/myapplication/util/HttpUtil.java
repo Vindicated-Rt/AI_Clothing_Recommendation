5c4f4f348e96d7865fa616fac43c94c8ee15de34
Vindicated-Rt <32959474+vindicated-rt@users.noreply.github.com>
2019-03-26 22:51:37 +0800

    add Http postRequest another method

--- app/src/main/java/vindicatedrt/com/myapplication/util/HttpUtil.java
+++ app/src/main/java/vindicatedrt/com/myapplication/util/HttpUtil.java
@@ -2,9 +2,12 @@ package vindicatedrt.com.myapplication.util;
 
 import android.util.Log;
 
+import com.alibaba.fastjson.JSON;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
@@ -27,7 +30,7 @@ public class HttpUtil {
         URL url = new URL(generalUrl);
         // 打开和URL之间的连接
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        Log.i(TAG,"打开链接，开始发送请求" + new Date().getTime() / 1000);
+        Log.i(TAG,"打开链接，开始发送请求");
         connection.setRequestMethod("POST");
         // 设置通用的请求属性
         connection.setRequestProperty("Content-Type", "application/json");
@@ -56,6 +59,41 @@ public class HttpUtil {
             in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "GBK"));
         else
             in = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
+        StringBuilder result = new StringBuilder();
+        String getLine;
+        while ((getLine = in.readLine()) != null) {
+            result.append(getLine);
+        }
+        in.close();
+        Log.i(TAG,"请求结束" + new Date().getTime() / 1000);
+        Log.i(TAG,"result:" + result);
+        return result.toString();
+    }
+
+    public static String post(String requestUrl, String requestInfo) throws Exception{
+        //根据链接建立请求
+        Log.i(TAG, "发送的链接Url："+requestUrl);
+        URL url = new URL(requestUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        //设置链接属性
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("Connection", "Keep-Alive");
+        connection.setUseCaches(false);
+        connection.setDoInput(true);
+        connection.setDoOutput(true);
+
+        //创建输入流
+        OutputStreamWriter osw = new OutputStreamWriter(connection.getOutputStream());
+        osw.write(requestInfo);
+        osw.flush();
+        osw.close();
+
+        //建立链接
+        connection.connect();
+
+        //创建输入流
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
         StringBuilder result = new StringBuilder();
         String getLine;
         while ((getLine = in.readLine()) != null) {
